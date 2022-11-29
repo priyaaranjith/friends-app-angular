d874f0fca59e005f6c7081eb721e71b1cf95f62e
priyaaranjith <ranjithpriya307@gmail.com>
2022-11-29 12:14:51 +0530

    service

--- src/app/add-friend/add-friend.component.ts
+++ src/app/add-friend/add-friend.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-friend.component.css']
 })
 export class AddFriendComponent {
+  name = ""
+  friendName = ""
+  DescribeYourFriend=""
+  friendNickName =""
+
+
+  readValues = () =>
+  {
+   let data:any = {"name":this.name,"friendName":this.friendName,"friendNickName":this.friendNickName,"DescribeYourFriend":this.DescribeYourFriend}
+   console.log(data)
+  }
 
 }
